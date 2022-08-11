19ca3c156e51f57f6e91150128c885a3c88102e8
Yurii-Rykov <103988434+Yurii-Rykov@users.noreply.github.com>
2022-08-11 13:03:40 +0300

    in-work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem'
 import LoadMore from './Button/Button';
 
 export default class App extends Component {
+  
   state = {
     hits: null,
     search: '',
@@ -14,22 +15,28 @@ export default class App extends Component {
     page: 1,
     gallery: []
   };
-
+  
   loadMore = () =>{
-    this.setState(prevState => ({page:prevState.page + 1}))
-    console.log(this.state.page);
-    this.componentDidMount(this.state.search);
+    // this.componentDidMount(this.state.search);
+    this.setState(prevState => ({page:prevState.page + 1, gallery: [...prevState.hits.hits, ...prevState.gallery]}), this.componentDidMount)
+  
+    
+    // this.setState(prevState => ({gallery: [...prevState.hits.hits, ...prevState.gallery]}))
   }
-  hendleFormSubmit = search => {
+  hendleFormSubmit(search){
     if(search !== this.state.search){
-    this.setState({ search, gallery:[], page: 1 });
-    console.log(this.state.search, search);
-    this.componentDidMount(search)
+    this.setState({ search, page: 1 }, this.componentDidMount);
+    console.log(this.state.page)
+    this.setState(prevState =>({gallery: [...prevState.hits.hits]}))
+    // console.log(this.state.search, search);
+    // this.componentDidMount(search)
   }
   };
   
-  componentDidMount(query = '' ) {
+  componentDidMount( ) {
+    console.log(this.state.page)
       this.setState({status: 'pending'});
+      const query = this.state.search || ''
 
       fetch(
         `https://pixabay.com/api/?q=${query}&page=${this.state.page}&key=28565156-d6a8869547fee06a320be5b89&image_type=photo&orientation=horizontal&per_page=12`
@@ -43,8 +50,9 @@ export default class App extends Component {
         );
       }).then(hits => {
         if(hits.total > 0){
-          // this.setState({hits, status: 'resolved'})
-          this.setState(prevState => ({gallery: [...hits.hits], hits, status: 'resolved'}))
+          this.setState({hits, status: 'resolved'})
+          
+          // this.setState(prevState => ({gallery: [...hits.hits, ...prevState.gallery]}))
         }else{
           this.setState({status: 'rejected'})
         }
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-const ImageGalleryItem = ({ propHits }) => {
-console.log(propHits);
-  return propHits.map(el => (
+class ImageGalleryItem extends React.Component {
+// console.log(propHits);
+// state = {
+//   gallery: []
+// }
+// constructor(props){
+//   super(props)
+//   console.log(this.state.gallery);
+//   const newGallery = [...this.state.gallery, ...props.propHits]
+//   console.log(newGallery);
+//   this.state= {gallery: newGallery}
+// }
+
+render(){
+  // console.log(this.props);
+  // const oldGallery = this.state.gallery || []
+  // console.log(oldGallery, this.props);
+  
+  // console.log(newGallery);
+ 
+  return this.props.propHits.map(el => (
     <li key={el.id} className="gallery-item">
       <img src={el.webformatURL} alt="" width="300" />
     </li>
   ));
-};
+}};
 
 export default ImageGalleryItem;
